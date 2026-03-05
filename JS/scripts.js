12b6e168294e6b75a32c91aa8f3eada9a6ecee13
Ruan-Marcelo <luzruan60@gmail.com>
2026-03-05 17:02:16 -0300

    correção do emailJs

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,19 +1,47 @@
-// const toggle = document.getElementById("theme-toggle");
-// const body = document.body;
-// const text = document.getElementById("mode-text");
+const toggle = document.getElementById("theme-toggle");
+const body = document.body;
+const text = document.getElementById("mode-text");
 
-// toggle.addEventListener("click", () => {
+toggle.addEventListener("click", () => {
+  body.classList.toggle("light-mode");
+  toggle.classList.toggle("active");
 
-//   body.classList.toggle("light-mode");
-//   toggle.classList.toggle("active");
+  if (body.classList.contains("light-mode")) {
+    text.innerText = "ON";
+  } else {
+    text.innerText = "OFF";
+  }
+});
 
-//   if(body.classList.contains("light-mode")){
-//     text.innerText = "ON";
-//   }else{
-//     text.innerText = "OFF";
-//   }
+// Serviço de email usando o emailJs
+emailjs.init("N9ziwX_xtVhMMNSX7");
 
-// });
+document
+  .getElementById("contact-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const now = new Date();
+    document.getElementById("time").value = now.toLocaleString();
+
+    emailjs.sendForm("service_jl668yi", "template_tpbm4li", this).then(
+      function () {
+        const alertBox = document.getElementById("alert-msg");
+
+        alertBox.classList.add("show");
+
+        setTimeout(() => {
+          alertBox.classList.remove("show");
+        }, 3000);
+
+        document.getElementById("contact-form").reset();
+      },
+
+      function (error) {
+        alert("Erro ao enviar: " + error.text);
+      },
+    );
+  });
 
 // Serviço de email com node.js
 // const nodemailer = require("nodemailer");
